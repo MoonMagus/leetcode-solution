a9f974f71517fcdbb8042f609e275b8964606ec7
月光巫师 <935865579@qq.com>
2017-08-04 23:05:24 +0800

    Plus one.

---
+++ Plus One.cpp
@@ -0,0 +1,65 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int c = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            digits[i] = digits[i] + c;
+            c = digits[i] / 10;
+            digits[i] = digits[i] % 10;
+        }
+
+        if (c == 1)
+            digits.insert(digits.begin(), 1);
+
+        return digits;
+    }
+};
+
+vector<vector<int>> initializeData(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<vector<int>> v = vector<vector<int>>(lineCount);
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        while (iss >> number)
+            v[i].push_back(number);
+    }
+
+    return v;
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+void printData(vector<vector<int>>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        for (int j = 0; j < v[i].size(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<int>> &v = initializeData();
+
+    Solution so;
+
+    releaseData();
+    return 0;
+}
