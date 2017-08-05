71b5e24815efd6f3ea74e094620984a44c71f747
月光巫师 <935865579@qq.com>
2017-08-05 16:55:53 +0800

    Update multipleTemplate with listnode.

--- multipleTemplate.cpp
+++ multipleTemplate.cpp
@@ -4,7 +4,16 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
 using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
 vector<vector<int>> initializeData(int lineCount = 1) {
     freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
@@ -23,11 +32,6 @@ vector<vector<int>> initializeData(int lineCount = 1) {
     return v;
 }
 
-void releaseData() {
-    fclose(stdin);         //关闭文件   
-    fclose(stdout);        //关闭文件   
-}
-
 void printData(vector<vector<int>>& v) {
     for (int i = 0; i < v.size(); ++i) {
         for (int j = 0; j < v[i].size(); ++j)
@@ -36,10 +40,56 @@ void printData(vector<vector<int>>& v) {
     }
 }
 
-int main() {
-    vector<vector<int>> &v = initializeData(2);
+vector<ListNode*> initializeList(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<ListNode*> listTable;
 
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        ListNode* head = NULL;
+        ListNode* cur = NULL;
+        while (iss >> number) {
+            ListNode *node = new ListNode(number);
+            if (head == NULL)
+                head = node;
+
+            if (cur)
+                cur->next = node;
+            cur = node;
+        }
+
+        listTable.push_back(head);
+    }
+
+    return listTable;
+}
+
+void printList(vector<ListNode*> listTable) {
+    for (int i = 0; i < listTable.size(); ++i) {
+        ListNode* cur = listTable[i];
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+int main() {
+    vector<vector<int>> &v = initializeData();
     printData(v);
+
+    vector<ListNode*> list = initializeList();
+    printList(list);
 
     releaseData();
     return 0;
