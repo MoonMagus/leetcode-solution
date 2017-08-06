049de130d8e7dd8feaa6331047dd5dd3b17c6e92
月光巫师 <935865579@qq.com>
2017-08-06 18:00:09 +0800

    Reverse Nodes in k-Group

---
+++ Reverse Nodes in k-Group.cpp
@@ -0,0 +1,133 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+vector<vector<int>> initializeData(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<vector<int>> v = vector<vector<int>>(lineCount);
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        while (iss >> number)
+            v[i].push_back(number);
+    }
+
+    return v;
+}
+
+void printData(vector<vector<int>>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        for (int j = 0; j < v[i].size(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+}
+
+vector<ListNode*> initializeList(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<ListNode*> listTable;
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        ListNode* head = NULL;
+        ListNode* cur = NULL;
+        while (iss >> number) {
+            ListNode *node = new ListNode(number);
+            if (head == NULL)
+                head = node;
+
+            if (cur)
+                cur->next = node;
+            cur = node;
+        }
+
+        listTable.push_back(head);
+    }
+
+    return listTable;
+}
+
+void printList(vector<ListNode*> listTable) {
+    for (int i = 0; i < listTable.size(); ++i) {
+        ListNode* cur = listTable[i];
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+// *********************************************************************************************************
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (head == NULL || k <= 1)
+            return head;
+
+        // Caculate reverse region count.
+        int n = 0;
+        ListNode* cur = head;
+        while (cur) {
+            cur = cur->next;
+            ++n;
+        }
+        int m = n / k;
+
+        ListNode guard(-1);
+        ListNode* last = &guard;
+        guard.next = head;
+        ListNode* rear = last->next;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 1; j < k; ++j) {
+                ListNode* cur = rear->next;
+                rear->next = cur->next;
+                cur->next = last->next;
+                last->next = cur;
+            }
+
+            last = rear;
+            rear = last->next;
+        }
+
+        return guard.next;
+    }
+};
+
+int main() {
+    vector<ListNode*> list = initializeList();
+
+    vector<ListNode*> result;
+    Solution so;
+    result.push_back(so.reverseKGroup(list[0], 1));
+    printList(result);
+
+    releaseData();
+    return 0;
+}
