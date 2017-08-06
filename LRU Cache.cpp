2157ec14dec2e15014f6669caff64f89a576188f
月光巫师 <935865579@qq.com>
2017-08-06 23:58:04 +0800

    LRU Cache

---
+++ LRU Cache.cpp
@@ -0,0 +1,160 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <list>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+vector<vector<int>> initializeData(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<vector<int>> v = vector<vector<int>>(lineCount);
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        while (iss >> number)
+            v[i].push_back(number);
+    }
+
+    return v;
+}
+
+void printData(vector<vector<int>>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        for (int j = 0; j < v[i].size(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+}
+
+vector<ListNode*> initializeList(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<ListNode*> listTable;
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        ListNode* head = NULL;
+        ListNode* cur = NULL;
+        while (iss >> number) {
+            ListNode *node = new ListNode(number);
+            if (head == NULL)
+                head = node;
+
+            if (cur)
+                cur->next = node;
+            cur = node;
+        }
+
+        listTable.push_back(head);
+    }
+
+    return listTable;
+}
+
+void printList(vector<ListNode*> listTable) {
+    for (int i = 0; i < listTable.size(); ++i) {
+        ListNode* cur = listTable[i];
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+// *********************************************************************************************************
+class LRUCache {
+private:
+    struct cacheNode {
+        int key;
+        int value;
+        cacheNode(int k, int v) : key(k), value(v) {}
+    };
+
+    unordered_map<int, list<cacheNode>::iterator> cacheMap;
+    list<cacheNode> cacheList;
+    int cacheSize;
+
+public:
+    LRUCache(int capacity) {
+        cacheSize = capacity;
+    }
+
+    int get(int key) {
+        // Search.
+        if (cacheMap.find(key) != cacheMap.end()) {
+            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+            cacheMap[key] = cacheList.begin();
+            return cacheMap[key]->value;
+        }
+        else
+            return -1;
+    }
+
+    void put(int key, int value) {
+        // Add, delete and edit.
+        // IF element exist, then edit.
+        if (cacheMap.find(key) != cacheMap.end()) {
+            cacheMap[key]->value = value;
+            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+            cacheMap[key] = cacheList.begin();
+        }
+        // Add and delete.
+        else {
+            if (cacheMap.size() == cacheSize) {
+                // Remove the oldest in link and remove from map.
+                cacheMap.erase(cacheList.back().key);
+                cacheList.pop_back();
+            }
+
+            // Add the newest one to link and map.
+            cacheList.push_front(cacheNode(key, value));
+            cacheMap[key] = cacheList.begin();
+        }
+    }
+};
+
+/**
+* Your LRUCache object will be instantiated and called as such:
+* LRUCache obj = new LRUCache(capacity);
+* int param_1 = obj.get(key);
+* obj.put(key,value);
+*/
+int main() {
+    vector<ListNode*> list = initializeList();
+
+
+
+
+    releaseData();
+    return 0;
+}
