7f403bfef76d8c2cfc5854b2b816e623c03e3c8b
月光巫师 <935865579@qq.com>
2017-08-08 21:54:42 +0800

    Merge k Sorted Lists

---
+++ Merge k Sorted Lists.cpp
@@ -0,0 +1,159 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+vector<vector<int>> initializeData(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<vector<int>> v = vector<vector<int>>(lineCount);
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        while (iss >> number)
+            v[i].push_back(number);
+    }
+
+    return v;
+}
+
+void printData(vector<vector<int>>& v) {
+    for (int i = 0; i < v.length(); ++i) {
+        for (int j = 0; j < v[i].length(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+}
+
+vector<ListNode*> initializeList(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<ListNode*> listTable;
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        ListNode* head = NULL;
+        ListNode* cur = NULL;
+        while (iss >> number) {
+            ListNode *node = new ListNode(number);
+            if (head == NULL)
+                head = node;
+
+            if (cur)
+                cur->next = node;
+            cur = node;
+        }
+
+        listTable.push_back(head);
+    }
+
+    return listTable;
+}
+
+void printList(vector<ListNode*> listTable) {
+    for (int i = 0; i < listTable.size(); ++i) {
+        ListNode* cur = listTable[i];
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+// *********************************************************************************************************
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return merge(begin(lists), end(lists));
+    }
+
+    ListNode* merge(vector<ListNode*>::iterator start, vector<ListNode*>::iterator end) {
+        if (start >= end)
+            return NULL;
+        
+        if (start + 1 == end)
+            return *start;
+
+        int n = distance(start, end);
+        ListNode* l1 = merge(start, next(start, n / 2 + 1));
+        ListNode* l2 = merge(next(start, n / 2 + 1), end);
+
+        // Merge two sorted list.
+        ListNode guard(-1);
+        ListNode* rear = &guard;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                rear->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                rear->next = l2;
+                l2 = l2->next;
+            }
+        
+            rear = rear->next;
+        }
+
+        ListNode* l = l1 ? l1 : l2;
+        while (l) {
+            rear->next = l;
+            l = l->next;
+            rear = rear->next;
+        }
+
+        return guard.next;
+    }
+};
+
+int main() {
+    vector<ListNode*> list = initializeList();
+
+    releaseData();
+    return 0;
+}
