14aacb2b39557e9bf64a78f991b88a81f14b01f9
月光巫师 <935865579@qq.com>
2017-08-15 02:09:51 +0800

    Longest Palindromic Substring

---
+++ Longest Palindromic Substring.cpp
@@ -0,0 +1,139 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <list>
+#include <stack>
+#include <queue>
+#include <unordered_Set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+vector<vector<int>> initializeData(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<vector<int>> v = vector<vector<int>>(lineCount);
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        while (iss >> number)
+            v[i].push_back(number);
+    }
+
+    return v;
+}
+
+void printData(vector<vector<int>>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        for (int j = 0; j < v[i].size(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+}
+
+vector<ListNode*> initializeList(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<ListNode*> listTable;
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        ListNode* head = NULL;
+        ListNode* cur = NULL;
+        while (iss >> number) {
+            ListNode *node = new ListNode(number);
+            if (head == NULL)
+                head = node;
+
+            if (cur)
+                cur->next = node;
+            cur = node;
+        }
+
+        listTable.push_back(head);
+    }
+
+    return listTable;
+}
+
+void printList(vector<ListNode*> listTable) {
+    for (int i = 0; i < listTable.size(); ++i) {
+        ListNode* cur = listTable[i];
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        const int ls = s.size();
+        vector<bool> p(ls, false);
+
+        string longest;
+        for (int i = ls - 1; i >= 0; --i) {
+            for (int j = ls - 1; j >= i; --j) {
+                if (s[i] == s[j] && (j - i <= 2 || p[j - 1])) {
+                    p[j] = true;
+                    if (j - i + 1 > longest.size())
+                        longest = s.substr(i, j - i + 1);
+                }
+                else
+                    p[j] = false;
+            }
+        }
+
+        return longest;
+    }
+};
+
+int main() {
+    vector<vector<int>> v = initializeData(1);
+    Solution so;
+
+    releaseData();
+    return 0;
+}
