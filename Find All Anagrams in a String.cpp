ba2ae0f5681292ca9ece3ebae3c7438cf38d929f
月光巫师 <935865579@qq.com>
2017-08-15 19:40:23 +0800

    Find All Anagrams in a String

---
+++ Find All Anagrams in a String.cpp
@@ -0,0 +1,161 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <list>
+#include <stack>
+#include <queue>
+#include <unordered_Set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+vector<vector<int>> initializeData(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<vector<int>> v = vector<vector<int>>(lineCount);
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        while (iss >> number)
+            v[i].push_back(number);
+    }
+
+    return v;
+}
+
+void printData(vector<vector<int>>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        for (int j = 0; j < v[i].size(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+}
+
+vector<ListNode*> initializeList(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<ListNode*> listTable;
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        ListNode* head = NULL;
+        ListNode* cur = NULL;
+        while (iss >> number) {
+            ListNode *node = new ListNode(number);
+            if (head == NULL)
+                head = node;
+
+            if (cur)
+                cur->next = node;
+            cur = node;
+        }
+
+        listTable.push_back(head);
+    }
+
+    return listTable;
+}
+
+void printList(vector<ListNode*> listTable) {
+    for (int i = 0; i < listTable.size(); ++i) {
+        ListNode* cur = listTable[i];
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+class Solution {
+private:
+    static const int ASCII = 256;
+    bool isAnagram(vector<int>& count_a, vector<int>& count_b) {
+        for (int i = 0; i < ASCII; ++i)
+            if (count_a[i] != count_b[i])
+                return false;
+
+        return true;
+    }
+
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> result;
+        const int ls = s.size();
+        const int lp = p.size();
+        if (ls < lp)
+            return result;
+
+        vector<int> count_a(ASCII, 0);
+        vector<int> count_b(ASCII, 0);
+        for (int i = 0; i < lp; ++i) {
+            count_a[p[i]]++;
+            count_b[s[i]]++;
+        }
+
+        for (int i = 0; i + lp - 1 < ls; ++i) {
+            if (i > 0) {
+                count_b[s[i - 1]]--;
+                count_b[s[i + lp - 1]]++;
+            }
+
+            if (isAnagram(count_a, count_b))
+                result.push_back(i);
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    vector<vector<int>> v = initializeData(1);
+    Solution so;
+    vector<int> result = so.findAnagrams("abab", "ab");
+
+    for (auto num : result)
+        cout << num << " ";
+    cout << endl;
+
+    releaseData();
+    return 0;
+}
