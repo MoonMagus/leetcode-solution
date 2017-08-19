b2e352f8c5a2350833f154f977d80eecd462f172
月光巫师 <935865579@qq.com>
2017-08-20 03:18:12 +0800

    Kth Largest Element in an Array

---
+++ Kth Largest Element in an Array.cpp
@@ -0,0 +1,172 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <list>
+#include <stack>
+#include <queue>
+#include <unordered_Set>
+#include <exception>
+#include <cstdlib>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+vector<vector<int>> initializeData(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<vector<int>> v = vector<vector<int>>(lineCount);
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        while (iss >> number)
+            v[i].push_back(number);
+    }
+
+    return v;
+}
+
+void printData(vector<vector<int>>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        for (int j = 0; j < v[i].size(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+}
+
+vector<ListNode*> initializeList(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<ListNode*> listTable;
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        ListNode* head = NULL;
+        ListNode* cur = NULL;
+        while (iss >> number) {
+            ListNode *node = new ListNode(number);
+            if (head == NULL)
+                head = node;
+
+            if (cur)
+                cur->next = node;
+            cur = node;
+        }
+
+        listTable.push_back(head);
+    }
+
+    return listTable;
+}
+
+void printList(vector<ListNode*> listTable) {
+    for (int i = 0; i < listTable.size(); ++i) {
+        ListNode* cur = listTable[i];
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+// T: O(n), S: O(1).
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        return findKthLargest(begin(nums), nums.size(), k);
+    }
+
+private:
+    int findKthLargest(vector<int>::iterator A, int n, int k) {
+        if (k <= 0 || n < k)
+            throw exception("Invalid input: n should no less than k and k should bigger than -1");
+
+        int position = partition(A, n);
+        if (position == k)
+            return A[position - 1];
+        else if (position < k)
+            return findKthLargest(next(A, position), n - position, k - position);
+        else
+            return findKthLargest(A, position - 1, k);
+    }
+
+    int partition(vector<int>::iterator A, int n) {
+        // int index = rand() % n;
+        // swap(A[n - 1], A[index]);
+        int key = A[n - 1];
+
+        // [0, i] >= key, [i + 1, j - 1] < key.
+        int i = -1;
+        for (int j = 0; j < n; ++j) {
+            if (A[j] >= key) {
+                ++i;
+                swap(A[i], A[j]);
+            }
+        }
+
+        return i + 1;
+    }
+};
+
+int main() {
+    vector<vector<int>> v = initializeData(1);
+    Solution so;
+    try {
+        int k = so.findKthLargest(v[0], 1);
+
+        cout << k << endl;
+    }
+    catch (const exception& e) {
+        cout << e.what() << endl;
+    }
+
+    releaseData();
+    return 0;
+}
