224d0c04738a6e370efba423076fc15c1cb3ee6b
月光巫师 <935865579@qq.com>
2017-08-19 17:13:21 +0800

    V2. Binary Tree Maximum Path Sum

--- Binary Tree Maximum Path Sum.cpp
+++ Binary Tree Maximum Path Sum.cpp
@@ -107,30 +107,36 @@ void releaseData() {
 }
 
 // *********************************************************************************************************
+// Time: O(n), Space: O(logn)
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        max_sum = INT_MIN;
+        //if (root == NULL)
+        //   throw exception("Invalid input: empty root node!!");
+
         dfs(root);
 
-        return max_sum;
+        return max_mum;
     }
 
 private:
-    int max_sum;
-    // Return max path which start from root.
-    int dfs(const TreeNode* root) {
+    int max_mum = INT_MIN;
+
+    // Return max path start from root, which could end at any node of related subtree.s
+    int dfs(TreeNode* root) {
         if (root == NULL)
             return 0;
-        
+
+        int sum = root->val;
         int l = dfs(root->left);
         int r = dfs(root->right);
-        int sum = root->val;
         if (l > 0)
             sum += l;
+
         if (r > 0)
             sum += r;
-        max_sum = max(max_sum, sum);
+
+        max_mum = max(max_mum, sum);
 
         return max(l, r) > 0 ? max(l, r) + root->val : root->val;
     }
