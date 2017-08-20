80981ac0e819b347baeed64127ed8ed0104c1fc5
月光巫师 <935865579@qq.com>
2017-08-21 01:06:58 +0800

    Single Number

--- Single Number.cpp
+++ Single Number.cpp
@@ -7,17 +7,45 @@
 #include <unordered_map>
 #include <algorithm>
 #include <numeric>
+#include <list>
+#include <stack>
+#include <queue>
+#include <unordered_Set>
+#include <exception>
+#include <cstdlib>
+#include <set>
 using namespace std;
 
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int single = 0;
-        for (auto n : nums)
-            single ^= n;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
-        return single;
-    }
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
 };
 
 vector<vector<int>> initializeData(int lineCount = 1) {
@@ -37,11 +65,6 @@ vector<vector<int>> initializeData(int lineCount = 1) {
     return v;
 }
 
-void releaseData() {
-    fclose(stdin);         //关闭文件   
-    fclose(stdout);        //关闭文件   
-}
-
 void printData(vector<vector<int>>& v) {
     for (int i = 0; i < v.size(); ++i) {
         for (int j = 0; j < v[i].size(); ++j)
@@ -50,11 +73,75 @@ void printData(vector<vector<int>>& v) {
     }
 }
 
-int main() {
-    vector<vector<int>> &v = initializeData();
+vector<ListNode*> initializeList(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<ListNode*> listTable;
 
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        ListNode* head = NULL;
+        ListNode* cur = NULL;
+        while (iss >> number) {
+            ListNode *node = new ListNode(number);
+            if (head == NULL)
+                head = node;
+
+            if (cur)
+                cur->next = node;
+            cur = node;
+        }
+
+        listTable.push_back(head);
+    }
+
+    return listTable;
+}
+
+void printList(vector<ListNode*> listTable) {
+    for (int i = 0; i < listTable.size(); ++i) {
+        ListNode* cur = listTable[i];
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+// T: O(n), S: O(1).
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        const int n = nums.size();
+        /*if (n == 0)
+        throw exception("Invalid input");*/
+
+        int single = 0;
+        for (auto num : nums)
+            single ^= num;
+
+        return single;
+    }
+};
+
+int main() {
+    vector<vector<int>> v = initializeData(1);
     Solution so;
-    cout << so.singleNumber(v[0]) << endl;
+    try {
+        cout << so.singleNumber(v[0]) << endl;
+    }
+    catch (const exception& e) {
+        cout << e.what() << endl;
+    }
 
     releaseData();
     return 0;
