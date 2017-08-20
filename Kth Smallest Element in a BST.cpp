18419f7ae6e6d48f50c66efb824ee6899d554ef8
月光巫师 <935865579@qq.com>
2017-08-21 02:57:40 +0800

    Kth Smallest Element in a BST

---
+++ Kth Smallest Element in a BST.cpp
@@ -0,0 +1,169 @@
+﻿#pragma warning(disable:4996)
+#include <cstdio>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <list>
+#include <stack>
+#include <queue>
+#include <unordered_Set>
+#include <exception>
+#include <cstdlib>
+#include <set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+vector<vector<int>> initializeData(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<vector<int>> v = vector<vector<int>>(lineCount);
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        while (iss >> number)
+            v[i].push_back(number);
+    }
+
+    return v;
+}
+
+void printData(vector<vector<int>>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        for (int j = 0; j < v[i].size(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+}
+
+vector<ListNode*> initializeList(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<ListNode*> listTable;
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        ListNode* head = NULL;
+        ListNode* cur = NULL;
+        while (iss >> number) {
+            ListNode *node = new ListNode(number);
+            if (head == NULL)
+                head = node;
+
+            if (cur)
+                cur->next = node;
+            cur = node;
+        }
+
+        listTable.push_back(head);
+    }
+
+    return listTable;
+}
+
+void printList(vector<ListNode*> listTable) {
+    for (int i = 0; i < listTable.size(); ++i) {
+        ListNode* cur = listTable[i];
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+// T: O(n), S: O(logn).
+// Solution: Inorder traversal.
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        //if (root == NULL || k <= 0)
+        //    throw exception("Invalid input");
+
+        int checked = 0;
+        int result;
+        kthSmall(root, k, checked, result);
+
+        //if (checked != k)
+        //    throw exception("Tree nodes count less than k");
+
+        return result;
+    }
+
+private:
+    void kthSmall(TreeNode* root, int k, int& checked, int& result) {
+        // Check left subtree.
+        if (root->left)
+            kthSmall(root->left, k, checked, result);
+        
+        // Check root node.
+        checked++;
+        if (checked == k) {
+            result = root->val;
+
+            return;
+        }
+
+        // Check right tree.
+        if (root->right)
+            kthSmall(root->right, k, checked, result);
+    }
+};
+
+int main() {
+    vector<vector<int>> v = initializeData(1);
+    Solution so;
+    try {
+    }
+    catch (const exception& e) {
+        cout << e.what() << endl;
+    }
+
+    releaseData();
+    return 0;
+}
